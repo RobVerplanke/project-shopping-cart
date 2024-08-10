4ace91d797c74471c819601c4880cdbad038cb2d
Rob <rverplanke@gmail.com>
2024-08-10 22:22:21 +0200

    build: display items in cart page

--- src/components/pages/CartPage.jsx
+++ src/components/pages/CartPage.jsx
@@ -1,5 +1,53 @@
+import React from 'react';
+import { useData } from '../../context/DataContext';
+import '../../styles/CartPage.css';
+
 function Cart() {
-  return <h2>Cart page</h2>;
+  const { cartItems } = useData();
+
+  return (
+    <>
+      <h2>Cart page</h2>
+      <table>
+        <caption>Products in your cart</caption>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th></th>
+            <th>Costs</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map((item) => {
+            return (
+              <React.Fragment key={item.id}>
+                <tr>
+                  <td rowSpan={3}>
+                    <img src={item.image} alt="Product image" />
+                  </td>
+                  <td>{item.title}</td>
+                </tr>
+                <tr>
+                  <td>{item.description}</td>
+                </tr>
+                <tr>
+                  <td>Amount: {item.quantity}</td>
+                  <td>{item.price}</td>
+                </tr>
+              </React.Fragment>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td></td>
+            <td>Total costs</td>
+            <td>234,00</td>
+          </tr>
+        </tfoot>
+      </table>
+    </>
+  );
 }
 
 export default Cart;
