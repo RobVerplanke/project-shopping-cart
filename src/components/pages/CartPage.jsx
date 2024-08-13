27a600a6294f6da7dd5b050b047479b9b8bdf8e6
Rob <rverplanke@gmail.com>
2024-08-13 15:12:22 +0200

    build: add option to remove items in the cart
    Also fix layout of the table that contains all items in the cart

--- src/components/pages/CartPage.jsx
+++ src/components/pages/CartPage.jsx
@@ -3,8 +3,16 @@ import { useData } from '../../context/DataContext';
 import '../../styles/CartPage.css';
 
 function Cart() {
-  const { cartItems } = useData();
+  const { cartItems, setCartItems } = useData();
   let totalCosts = 0;
+
+  // Show message if shopping cart is empty instead of render a empty table
+  if (!cartItems.length) return <p>No items in your schopping cart</p>;
+
+  // Return a new array in which the corresponding item is filtered out
+  function handleRemove(itemIdToRemove) {
+    setCartItems(cartItems.filter((item) => item.id != itemIdToRemove));
+  }
 
   return (
     <>
@@ -15,35 +23,53 @@ function Cart() {
           <tr>
             <th>Product</th>
             <th></th>
+            <th></th>
+            <th></th>
             <th>Costs</th>
           </tr>
         </thead>
+
         <tbody>
           {cartItems.map((item) => {
-            totalCosts += item.price * item.quantity;
+            totalCosts += item.price * item.quantity; // Accumulate the total costs
             return (
               <React.Fragment key={item.id}>
-                <tr>
-                  <td rowSpan={3}>
+                <tr rowSpan={3}>
+                  <td>
                     <img src={item.image} alt="Product image" />
                   </td>
-                  <td>{item.title}</td>
-                </tr>
-                <tr>
-                  <td>{item.description}</td>
-                </tr>
-                <tr>
-                  <td>Amount: {item.quantity}</td>
-                  <td>{item.price * item.quantity}</td>
+                  <td>
+                    {item.title}
+                    <br></br>
+                    {item.description}
+                    <br></br>
+                    Amount: {item.quantity}
+                  </td>
+                  <td>
+                    <br></br>
+                    <br></br>
+                    <button onClick={() => handleRemove(item.id)}>
+                      Remove
+                    </button>
+                  </td>
+                  <td></td>
+                  <td>
+                    <br></br>
+                    <br></br>
+                    {item.price * item.quantity}
+                  </td>
                 </tr>
               </React.Fragment>
             );
           })}
         </tbody>
+
         <tfoot>
           <tr>
             <td></td>
-            <td rowSpan={3}>Total</td>
+            <td></td>
+            <td>Total</td>
+            <td></td>
             <td>{totalCosts.toFixed(2)}</td>
           </tr>
         </tfoot>
