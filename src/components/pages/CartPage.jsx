c5532d9c8b496594cf345880a3de12d3bcd29bca
Rob <rverplanke@gmail.com>
2024-08-13 20:27:00 +0200

    fix: improve style on cart table

--- src/components/pages/CartPage.jsx
+++ src/components/pages/CartPage.jsx
@@ -9,13 +9,13 @@ function Cart() {
   // Show message if shopping cart is empty instead of render a empty table
   if (!cartItems.length) return <p>No items in your schopping cart</p>;
 
-  // Return a new array in which the corresponding item is filtered out
+  // Return a new array in which the item with a corresponding id is filtered out
   function handleRemove(itemIdToRemove) {
     setCartItems(cartItems.filter((item) => item.id != itemIdToRemove));
   }
 
   return (
-    <>
+    <main>
       <h2>Cart page</h2>
       <table>
         <caption>Products in your cart</caption>
@@ -25,7 +25,7 @@ function Cart() {
             <th></th>
             <th></th>
             <th></th>
-            <th>Costs</th>
+            <th id="heading-costs">Costs</th>
           </tr>
         </thead>
 
@@ -35,28 +35,28 @@ function Cart() {
             return (
               <React.Fragment key={item.id}>
                 <tr rowSpan={3}>
-                  <td>
+                  <td id="image-column">
                     <img src={item.image} alt="Product image" />
                   </td>
                   <td>
-                    {item.title}
-                    <br></br>
-                    {item.description}
-                    <br></br>
-                    Amount: {item.quantity}
+                    <div className="product-info">
+                      <span className="product-title">{item.title}</span>
+                      <span className="product-description">
+                        {item.description}
+                      </span>
+                      <span className="product-quantity">
+                        Amount: {item.quantity}
+                      </span>
+                    </div>
                   </td>
                   <td>
-                    <br></br>
-                    <br></br>
                     <button onClick={() => handleRemove(item.id)}>
                       Remove
                     </button>
                   </td>
                   <td></td>
-                  <td>
-                    <br></br>
-                    <br></br>
-                    {item.price * item.quantity}
+                  <td className="product-total">
+                    € {(item.price * item.quantity).toFixed(2)}
                   </td>
                 </tr>
               </React.Fragment>
@@ -68,13 +68,13 @@ function Cart() {
           <tr>
             <td></td>
             <td></td>
-            <td>Total</td>
+            <td className="cart-total">Total</td>
             <td></td>
-            <td>{totalCosts.toFixed(2)}</td>
+            <td className="cart-total-price">€ {totalCosts.toFixed(2)}</td>
           </tr>
         </tfoot>
       </table>
-    </>
+    </main>
   );
 }
 
