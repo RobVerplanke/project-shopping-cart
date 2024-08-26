4476f7204ae3ac92c767aef33de537c07addebef
Rob <rverplanke@gmail.com>
2024-08-26 19:37:05 +0200

    refactor: use async await method

--- src/api/shopApi.js
+++ src/api/shopApi.js
@@ -1,15 +1,20 @@
 import { BASE_URL, MAX_ITEMS } from '../lib/constants';
 
-export default function getShopData(setItems, setError, setIsLoading) {
-  fetch(`${BASE_URL}?limit=${MAX_ITEMS}`)
-    .then((response) => response.json())
-    .then((response) => {
-      setItems(response);
-      setError(null);
-    })
-    .catch((error) => {
-      setError(error);
-      setItems(null);
-    })
-    .finally(() => setIsLoading(false));
+export async function getShopData(setItems, setError, setIsLoading) {
+  try {
+    const response = await fetch(`${BASE_URL}?limit=${MAX_ITEMS}`);
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const data = await response.json();
+    setItems(data);
+    setError(null);
+  } catch (error) {
+    setError(error);
+    setItems(null);
+  } finally {
+    setIsLoading(false);
+  }
 }
